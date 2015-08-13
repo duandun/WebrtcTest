ab1df7f7c1459f264929cc39219d93fbdc75caec
cr <duandun@gmail.com>
2015-08-13 18:37:03 +0800

    fix a lot

--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,34 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
 		//判断访问终端
-	var browser={
-	    versions:function(){
-	      //  var u = navigator.userAgent, app = navigator.appVersion;
-	        var u = req.get('User-Agent');
-	        return {
-	            trident: u.indexOf('Trident') > -1, //IE内核
-	            presto: u.indexOf('Presto') > -1, //opera内核
-	            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-	            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
-	            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-	            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-	            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-	            iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
-	            iPad: u.indexOf('iPad') > -1, //是否iPad
-	            webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
-	            weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
-	            qq: u.match(/\sQQ/i) == " qq" //是否QQ
-	        };
-	    }(),
-	//    language:(navigator.browserLanguage || navigator.language).toLowerCase()
-	};
+	// var browser={
+	//     versions:function(){
+	//       //  var u = navigator.userAgent, app = navigator.appVersion;
+	//         var u = req.get('User-Agent');
+	//         return {
+	//             trident: u.indexOf('Trident') > -1, //IE内核
+	//             presto: u.indexOf('Presto') > -1, //opera内核
+	//             webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+	//             gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
+	//             mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+	//             ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+	//             android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+	//             iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
+	//             iPad: u.indexOf('iPad') > -1, //是否iPad
+	//             webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+	//             weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+	//             qq: u.match(/\sQQ/i) == " qq" //是否QQ
+	//         };
+	//     }(),
+	// //    language:(navigator.browserLanguage || navigator.language).toLowerCase()
+	// };
 
-	if (browser.versions.ios) {
-		res.render('fun');
-	}
-	if (browser.versions.android) {
-		res.render('uc');
-	}
+	// if (browser.versions.ios) {
+	// 	res.render('fun');
+	// }
+	// if (browser.versions.android) {
+	// 	res.render('uc');
+	// }
 
   res.render('index', { title: 'Express' });
 });
@@ -59,6 +59,10 @@ router.get('/loginok', function(req, res, next) {
 
 router.get('/uc', function(req, res, next) {
 	res.render('uc');
+});
+
+router.get('/gen', function(req, res, next) {
+	res.render('gen');
 });
 
 router.get('/mobile', function(req, res, next) {
